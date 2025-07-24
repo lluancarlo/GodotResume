53e0309579e6161b1e327096e267319a2ca84221
Luan <luanveloz@gmail.com>
2025-07-25 01:43:35 +0200

    fix: try fix browser errors

--- scripts/loading.gd
+++ scripts/loading.gd
@@ -10,22 +10,18 @@ var load_in_progress := false
 
 
 func _ready():
-	# Start loading the heavy scene in background
 	ResourceLoader.load_threaded_request(main_scene_path)
 	load_in_progress = true
-	print("Loading MainGame.tscn in background...")
 
 
-func _process(_delta):
+func _process(_delta: float) -> void:
 	if load_in_progress:
 		var status = ResourceLoader.load_threaded_get_status(main_scene_path)
 		if status == ResourceLoader.THREAD_LOAD_LOADED:
 			main_scene_resource = ResourceLoader.load_threaded_get(main_scene_path)
-			print("MainGame.tscn loaded and ready!")
 			load_in_progress = false
-			# You can now switch scenes when the player is ready
 		elif status == ResourceLoader.THREAD_LOAD_FAILED:
-			push_error("Failed to load main scene.")
+			print("Failed to load game.")
 			load_in_progress = false
 
 
