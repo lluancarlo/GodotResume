84d1638940da26610b5d57db63277aebdfa68492
Luan <luanveloz@gmail.com>
2025-07-25 02:32:46 +0200

    feat: remove loading scene

--- scripts/loading.gd
+++ scripts/loading.gd
@@ -4,14 +4,14 @@ class_name Loading
 
 @onready var _menu_language : Control = $SelectLanguage
 
-#var main_scene : PackedScene = preload("res://scenes/game.tscn")
+var main_scene : PackedScene = preload("res://scenes/game.tscn")
 
 
 func transition_to_game() -> void:
 	await create_tween().tween_property(_menu_language, "modulate:a", 0.0, 0.5).from(1.0).finished
 	_menu_language.hide()
 	
-	#get_tree().change_scene_to_packed(main_scene)
+	get_tree().change_scene_to_packed(main_scene)
 
 
 func _on_english_pressed() -> void:
